2e5b3eaa63e242f1e2d2d1c77fb173ad05cacebe
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-10-20 03:05:47 -0700

    Update My_Msgs.lua

--- plugins/My_Msgs.lua
+++ plugins/My_Msgs.lua
@@ -68,7 +68,7 @@ end
       local hashs = 'msgs:'..result.peer_id..':'..extra.msg.to.id
       local msgss = redis:get(hashs)
       local percent = msgss / r * 100
-    return send_large_msg(chatid, "نام شما : "..name.."\nتعداد پیام ها ارسالی توسط شما : "..msgss.." ("..round2(percent).."%)\n  تمام پیام های ارسال شده در گروه: "..r.."")
+    return send_large_msg(chatid, "<b>Yourname</b> : "..name.."\n<b>Number of Messages</b> : "..msgss.." ("..round2(percent).."%)\n  <b>All messages in This Supergroup</b>: "..r.."")
 
     end
 end
