3d2506e11f88faa62a7451a2e7d98fd0d854804a
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-10-20 09:55:06 -0700

    Update Plist.lua

--- plugins/Plist.lua
+++ plugins/Plist.lua
@@ -14,7 +14,7 @@ do
 local function run(msg, matches)
   if matches[1] == 'plist' or 'planlist' then
     send_document(get_receiver(msg), "/root/blackplus/data/me/payment.webp", ok_cb, false)
-      return "For more information please Contact this robot \n\n@BlackSupport_Bot"
+      return reply_msg(msg.id,"><b>Plans List For M A X I M U S +</b>\n\n<i>1 Month :</i><b>5000T</b>\n\n<i>2 Month:</i><b>8000T</b>\n\n<i>Unlimited:</i><b>15000T</b>\n___________________________________________________\n<b>You can ask</b>@teshne<b>To create you a group</b>", ok_cb, false)
 end
 end
 return {
